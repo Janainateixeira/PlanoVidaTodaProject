41971967d014ba299ea32576c0c6008b8257a1a1
Janaina <jmtannus@gmail.com>
2022-09-16 21:46:50 -0300

    iniciando projeto plano para a vida toda

---
+++ src/components/AppName/index.js
@@ -0,0 +1,19 @@
+import { Text, StyleSheet } from "react-native";
+
+/*export default function AppName({ children }) {
+    return (
+        <Text style={StyleSheet.appName}>
+            {children}
+        </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    appName: {
+      flex: 1,
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'red',
+    },
+});
+*/
